27a88742c6baacaa6880bec4398f547601329dac
Beloved100 <olaseindedan1@gmail.com>
2024-10-31 20:00:43 +0000

    Authentication migration to Database

--- GIO-14BlazorApp/Data/Migrations/20241031195241_InitialIdentitySchema.Designer.cs
+++ GIO-14BlazorApp/Data/Migrations/20241031195241_InitialIdentitySchema.Designer.cs
@@ -4,6 +4,7 @@ using GIO_14BlazorApp.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GIO_14BlazorApp.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241031195241_InitialIdentitySchema")]
+    partial class InitialIdentitySchema
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
