33576610cb4dd746afb05945d11d479fffa82d35
Beloved100 <olaseindedan1@gmail.com>
2024-11-04 18:38:21 +0000

    ClientDb

--- GIO-14APILibrary/Models/ClientDbModel.cs
+++ GIO-14APILibrary/Models/ClientDbModel.cs
@@ -6,5 +6,12 @@ namespace GIO_14APILibrary.Models
 {
 	public class ClientDbModel
 	{
-	}
+        public string? AuthId { get; set; }
+        public bool Archive { get; set; }
+        public string? Automation { get; set; }
+        public string? NameAddressDetails { get; set; }
+        public string? BankDetails { get; set; }
+        public string? AccountStatus { get; set; }
+        public string? OptionsLogsDetails { get; set; }
+    }
 }
